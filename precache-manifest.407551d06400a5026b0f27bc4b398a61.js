3159126370d4c6ce46c60b226aa48cd8e937c266
Jillian Breau <jmbreau@mun.ca>
2022-10-12 10:36:02 -0230

    Updates

---
+++ precache-manifest.407551d06400a5026b0f27bc4b398a61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb1c902ff39cc62cbae4e64775c7416",
+    "url": "/galaxy-sounds/index.html"
+  },
+  {
+    "revision": "402b383f318a46e337f7",
+    "url": "/galaxy-sounds/static/css/main.2d6db981.chunk.css"
+  },
+  {
+    "revision": "e90250f281f525b0e60b",
+    "url": "/galaxy-sounds/static/js/2.47244525.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/galaxy-sounds/static/js/2.47244525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "402b383f318a46e337f7",
+    "url": "/galaxy-sounds/static/js/main.baf489c5.chunk.js"
+  },
+  {
+    "revision": "ff693d69212028020713",
+    "url": "/galaxy-sounds/static/js/runtime-main.c6a65b2d.js"
+  },
+  {
+    "revision": "e8625f7b7a8e5acc70b55e8b4125925a",
+    "url": "/galaxy-sounds/static/media/galaxy-background.e8625f7b.jpg"
+  }
+]);
